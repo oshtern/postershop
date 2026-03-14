462e0952748618128769b27b85b432a1de00602a
Juan <jon.alvart@gmail.com>
2026-03-14 18:04:45 -0400

    Fix manual tests

--- playwright.config.js
+++ playwright.config.js
@@ -1,6 +1,9 @@
 // @ts-check
 import { defineConfig, devices } from '@playwright/test';
 
+process.env.USER_EMAIL    ??= 'testproject@tutamail.com';
+process.env.USER_PASSWORD ??= 'TestP20dec#';
+
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
@@ -27,7 +30,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: process.env.BASE_URL || 'http://localhost:4000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
